29565909e66a15ce240bfa1841ada2974c89b21b
Caleb Connolly <caleb@postmarketos.org>
2025-01-16 19:49:35 +0100

    postmarketos-initramfs: use proper configfs path
    Use the correct /sys/kernel/configfs path for configs, set up a symlink
    at boot to the old path for compatibility.
    
    This simplifies re-using some initramfs scripts from the rootfs.
    
    Signed-off-by: Caleb Connolly <caleb@postmarketos.org>

--- main/postmarketos-initramfs/init_functions.sh
+++ main/postmarketos-initramfs/init_functions.sh
@@ -8,7 +8,7 @@ PMOS_BOOT="${PMOS_BOOT:-}"
 PMOS_ROOT="${PMOS_ROOT:-}"
 SUBPARTITION_DEV="${SUBPARTITION_DEV:-}"
 
-CONFIGFS="/config/usb_gadget"
+CONFIGFS="/sys/kernel/config/usb_gadget"
 CONFIGFS_ACM_FUNCTION="acm.usb0"
 CONFIGFS_MASS_STORAGE_FUNCTION="mass_storage.0"
 HOST_IP="${unudhcpd_host_ip:-172.16.42.1}"
@@ -69,8 +69,9 @@ mount_proc_sys_dev() {
 	mount -t devtmpfs -o mode=0755,nosuid dev /dev || echo "Couldn't mount /dev"
 	mount -t tmpfs -o nosuid,nodev,mode=0755 run /run || echo "Couldn't mount /run"
 
-	mkdir /config
-	mount -t configfs -o nodev,noexec,nosuid configfs /config
+	mkdir -p /sys/kernel/config
+	mount -t configfs -o nodev,noexec,nosuid configfs /sys/kernel/config
+	ln -sf sys/kernel/config /config
 
 	# /dev/pts (needed for telnet)
 	mkdir -p /dev/pts

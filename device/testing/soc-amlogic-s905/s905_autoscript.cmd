7ca663edc56a0673b49d36f7ba46401f410a7ac0
Ferass El Hafidi <funderscore@postmarketos.org>
2025-01-31 20:41:18 +0000

    WIP: soc-amlogic-s905: add support for directly booting a kernel from legacy u-boot
    Signed-off-by: Ferass El Hafidi <funderscore@postmarketos.org>

--- device/testing/soc-amlogic-s905/s905_autoscript.cmd
+++ device/testing/soc-amlogic-s905/s905_autoscript.cmd
@@ -2,3 +2,11 @@ if fatload mmc 0 0x1000000 u-boot.bin; then go 0x1000000; fi;
 if fatload usb 0 0x1000000 u-boot.bin; then go 0x1000000; fi;
 if ext4load mmc 0 0x1000000 u-boot.bin; then go 0x1000000; fi;
 if ext4load usb 0 0x1000000 u-boot.bin; then go 0x1000000; fi;
+setenv pmos_env_addr 0x1040000
+setenv pmos_env_load 'fatload ${devtype} ${devnum} ${env_addr} uEnv.ini && env import -t ${env_addr} ${filesize}'
+setenv pmos_boot 'bootm ${loadaddr} - ${dtb_mem_addr}'
+setenv pmos_try_boot 'if fatload ${pmos_devtype} ${pmos_devnum} ${loadaddr} uImage; then fatload ${pmos_devtype} ${pmos_devnum} ${dtb_mem_addr} ${pmos_env_dtb} && run pmos_boot; fi'
+setenv pmos_devtype usb
+for pmos_devnum in 0 1 2 3 ; do run pmos_try_boot ; done
+setenv pmos_devtype mmc
+for pmos_devnum in 0 1 2 3 ; do run pmos_try_boot ; done

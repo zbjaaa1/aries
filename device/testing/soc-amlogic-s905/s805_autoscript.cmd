366188dd05c59bfaf2230c9034c5ac9db6025365
Zsolt Vadasz <zsolt_vadasz@protonmail.com>
2025-02-06 21:15:59 +0100

    device-amlogic-tvbox-s8xx: Fix initramfs generation

--- device/testing/soc-amlogic-s905/s805_autoscript.cmd
+++ device/testing/soc-amlogic-s905/s805_autoscript.cmd
@@ -1,5 +1,5 @@
 fatload mmc 0:1 0x21000000 uImage
-fatload mmc 0:1 0x22000000 initramfs.uImage
+fatload mmc 0:1 0x22000000 uInitrd
 fatload mmc 0:1 0x21800000 dtbs/meson8m2-mxiii-plus.dtb
 setenv bootargs "console=ttyAML0,115200"
 bootm 0x21000000 0x22000000 0x21800000
